73bf13818959cd5725c58c434b0d9729c1d9e920
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-05-02 21:40:05 -0300

    feat: adapting feature component

--- components/ui/Features.tsx
+++ components/ui/Features.tsx
@@ -1,22 +1,13 @@
-import Icon, {
-  AvailableIcons,
-} from "deco-sites/fashion/components/ui/Icon.tsx";
-import Text from "deco-sites/fashion/components/ui/Text.tsx";
 import Container from "deco-sites/fashion/components/ui/Container.tsx";
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import Image from "deco-sites/std/components/Image.tsx";
 
 export interface Feature {
   /**
    * @description Image src
    */
-  icon: AvailableIcons;
-  /**
-   * @description Title
-   */
-  title: string;
-  /**
-   * @description Description and Image alt text
-   */
-  description: string;
+  image: LiveImage;
+  alt: string;
 }
 
 export interface Props {
@@ -27,26 +18,22 @@ function FeatureHighlights(
   { features }: Props,
 ) {
   return (
-    <Container class="min-h-[280px] p-6 sm:px-0 sm:py-10">
-      <div class="border-base-200 border border-solid">
-        <div class="flex flex-col justify-evenly divide-y divide-base-200 mx-6 sm:flex-row sm:divide-y-0 sm:divide-x sm:mx-0 sm:my-10">
-          {features.map(({ icon: id = "Truck", title, description }) => (
-            <div class="flex flex-row gap-4 py-6 sm:flex-col sm:py-0 sm:px-8">
-              <Icon
-                id={id}
-                width={40}
-                height={40}
-                strokeWidth={2}
-              />
-              <div class="flex flex-col gap-2">
-                <Text variant="heading-3">{title}</Text>
-                <Text variant="caption">
-                  {description}
-                </Text>
-              </div>
-            </div>
+    <Container class="py-2 lg:py-5">
+      {/* desktop */}
+      <div class="hidden  border-base-200 border border-solid rounded-lg h-[63px] lg:flex items-center overflow-hidden relative after:absolute after:h-full after:w-2 after:bg-primary after:right-0  before:absolute before:h-full before:w-2 before:bg-primary shadow-default">
+        <div class="flex justify-around items-center px-5 flex-grow">
+          {features.map(({ image, alt }) => (
+            <Image src={image} alt={alt} width={190} height={42} />
           ))}
         </div>
+      </div>
+      {/* mobile */}
+      <div class="lg:hidden flex items-center justify-around gap-4 px-4">
+        {features.filter((item, index) => index < 2).map(({ image, alt }) => (
+          <div class="bg-camp-gray flex-grow p-4 border border-gray-border rounded-lg flex items-center justify-center">
+            <Image src={image} alt={alt} width={328} height={115} />
+          </div>
+        ))}
       </div>
     </Container>
   );
